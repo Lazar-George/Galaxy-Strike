d9a09dc8b2969aba6182cec6dd47088ccef6482c
Lazar-George <george.lazar_504@student.upit.ro>
2025-06-25 23:00:02 +0300

    Features 25-30

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,9 +3,29 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject destroyedVfx;
+    [SerializeField] int hitpoints;
+    [SerializeField] int scorevalue;
+    Scoreboard scoreboard;
+
+    private void Start()
+    {
+        scoreboard = FindFirstObjectByType<Scoreboard>();
+    }
    private void OnParticleCollision(GameObject other)
     {
-        Instantiate(destroyedVfx,transform.position,Quaternion.identity);
-        Destroy(this.gameObject);
+        ProcessHit();
+    }
+
+    private void ProcessHit()
+    {
+        
+        hitpoints--;
+        if ((hitpoints <= 0))
+        {
+            scoreboard.IncreaseScore(scorevalue);
+
+            Instantiate(destroyedVfx, transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+        }
     }
 }
